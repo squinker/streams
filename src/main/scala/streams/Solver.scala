6751e9680204ea56d2c47eb1771788289e6d01b6
William Bellamy <william.bellamy@itv.co.uk>
2016-11-04 18:31:51 +0000

    try to finish

--- src/main/scala/streams/Solver.scala
+++ src/main/scala/streams/Solver.scala
@@ -36,13 +36,13 @@ trait Solver extends GameDef {
   def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] = {
 
     def helper(neighbours: List[(Block, Move)]): Stream[(Block, List[Move])] = {
+
+      //TODO ensure neighbours are legal here
       neighbours match {
         case x :: xs =>
-
-          println("** x" + x)
           (x._1, x._2 :: history ) #:: helper(xs)
+
         case Nil     =>
-          println("** EMpty")
           Stream.empty
       }
     }
@@ -83,19 +83,44 @@ trait Solver extends GameDef {
    * construct the correctly sorted stream.
    */
   def from(initial: Stream[(Block, List[Move])],
-           explored: Set[Block]): Stream[(Block, List[Move])] = ???
+           explored: Set[Block]): Stream[(Block, List[Move])] = {
+
+    def helper(initial: Stream[(Block, List[Move])],
+               explored: Set[Block]): Stream[(Block, List[Move])] = {
+
+      initial match {
+        case Stream.Empty => Stream.Empty
+        case h #:: tl =>
+
+          for {
+            naysWithHist <- neighborsWithHistory(h._1, h._2)
+            thing        <- naysWithHist #:: helper(tl, explored union Set(naysWithHist._1))
+          } yield thing
+
+      }
+    }
+
+    helper(initial, explored)
+  }
 
   /**
    * The stream of all paths that begin at the starting block.
    */
-  lazy val pathsFromStart: Stream[(Block, List[Move])] = ???
+  lazy val pathsFromStart: Stream[(Block, List[Move])] = from(
+    (Block(startPos, startPos), Nil ) #:: Stream.empty,
+    Set()
+  )
 
   /**
    * Returns a stream of all possible pairs of the goal block along
    * with the history how it was reached.
    */
-  lazy val pathsToGoal: Stream[(Block, List[Move])] = ???
-
+  lazy val pathsToGoal: Stream[(Block, List[Move])] = {
+    for {
+      paths <- pathsFromStart
+      if paths._1 == goal
+    } yield paths
+  }
   /**
    * The (or one of the) shortest sequence(s) of moves to reach the
    * goal. If the goal cannot be reached, the empty list is returned.
@@ -104,5 +129,6 @@ trait Solver extends GameDef {
    * the first move that the player should perform from the starting
    * position.
    */
-  lazy val solution: List[Move] = ???
+  lazy val solution: List[Move] = pathsToGoal.take(10).head._2
+
 }
